2589fb4c80aa5c2984d7ed8ed712c5fb2cbf7f0b
luoandong <luoandon@gmail.com>
2021-11-07 09:06:44 +0800

    feat:添加api接口

--- src/main/java/com/alinesno/student/learn/demo/alinesnostudentdemo/AlinesnoStudentDemoApplication.java
+++ src/main/java/com/alinesno/student/learn/demo/alinesnostudentdemo/AlinesnoStudentDemoApplication.java
@@ -2,9 +2,17 @@ package com.alinesno.student.learn.demo.alinesnostudentdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AlinesnoStudentDemoApplication {
+
+	@GetMapping("/hello")
+	public String hello(String name){
+		return "hello , " + name ; 
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlinesnoStudentDemoApplication.class, args);
